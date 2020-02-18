9b44c42d0b5988a104256208d07762d454044987
WWAARR <wwaarrtop@gmail.com>
2020-02-18 21:23:48 +0200

    Add files via upload

---
+++ Car/Car/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Car
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Tank tank = new Tank(40);
+            Console.WriteLine(tank);
+        }
+    }
+}
---
+++ Car/Car/Tank.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Car
+{
+    class Tank
+    {
+        uint volume;//characteristics
+        double fuel;//object condition
+        public uint Volume{
+            get => volume;
+            private set => volume = value >= 40 && value <= 120 ? value : throw new Exception("Bad tank volume");
+        }
+        public double Fuel
+        {
+            get => fuel;
+            set
+            {
+                fuel -= value;
+                if (fuel < 0) fuel = 0;
+                if (fuel >volume) fuel = volume;
+
+            }
+        }
+
+        public Tank(uint volume)
+        {
+            Volume = volume;
+            Fuel = 0;
+        }
+
+        void Fill(double fuel)
+        {
+            Fuel += fuel;
+        }
+
+        public override string ToString()
+        {
+            return  $"Tank volume : {Volume};\nFuel level : {Fuel}";
+        }
+    }
+}
